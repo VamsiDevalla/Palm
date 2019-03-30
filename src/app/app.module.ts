09914ea1ecdd0924d9d469006c04b20b7eac3666
Vamsi Devalla <vamsidevalla@Vamsis-MacBook-Pro.local>
2019-03-30 18:27:14 -0400

    adding side nav adding secondary nav updating to nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrimaryHeaderComponent } from './components/primary-header/primary-header.component';
+import { SideNavComponent } from './components/side-nav/side-nav.component';
+import { SecondaryHeaderComponent } from './components/secondary-header/secondary-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrimaryHeaderComponent,
+    SideNavComponent,
+    SecondaryHeaderComponent
   ],
   imports: [
     BrowserModule,
