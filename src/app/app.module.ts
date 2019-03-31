02f8f7e728a8fff1bc38e44e853c76fbabaa24a5
Vamsi Devalla <vamsidevalla@Vamsis-MBP.home>
2019-03-31 16:20:18 -0400

    finishing tab styling and adding about tab

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { PrimaryHeaderComponent } from './components/primary-header/primary-head
 import { SideNavComponent } from './components/side-nav/side-nav.component';
 import { SecondaryHeaderComponent } from './components/secondary-header/secondary-header.component';
 import { ActionButtonsComponent } from './components/action-buttons/action-buttons.component';
+import { TabBarComponent } from './components/tab-bar/tab-bar.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { ActionButtonsComponent } from './components/action-buttons/action-butto
     PrimaryHeaderComponent,
     SideNavComponent,
     SecondaryHeaderComponent,
-    ActionButtonsComponent
+    ActionButtonsComponent,
+    TabBarComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
