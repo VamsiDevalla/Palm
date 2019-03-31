decaebd035cdd274bd35f24e757746f22d3c6997
Vamsi Devalla <vamsidevalla@Vamsis-MBP.home>
2019-03-31 17:22:56 -0400

    completing

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AboutService} from '../../services/about.service';
 
 @Component({
   selector: 'app-about',
@@ -6,10 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
-
-  constructor() { }
+  about: any;
+  constructor(private dataService: AboutService) { }
 
   ngOnInit() {
+    this.about = {};
+    this.dataService.getAboutData().subscribe(data => {
+      this.about.applicationName = data['applicationNumberText'];
+      this.about.filingDate = data['filingDate'];
+      this.about.continuityForeign = data['continuityForeign'] || 'N/A';
+      this.about.title = data['inventionTitle'];
+      this.about.status = `${data['status']} (${data['applicationStatusDate']})`;
+      this.about.Examiner = data['examinerName'];
+      this.about.ID = data['examinerNumber'];
+      this.about.GAU = data['gau'];
+      this.about.docketCategory = data['docketCategory'] || 'N/A';
+      this.about.speList = data['speList'].reduce((acc,ele) => acc + ele + ',' , '')|| 'N/A';
+      this.about.inventorNames = data['inventorNames'].reduce((acc,ele) => acc + ele + ',', '') || 'N/A';
+      this.about.applicantNames = data['applicantNames'] || 'N/A';
+      this.about.lawFrim = data['lawFrim'] || 'N/A';
+      this.about.applicationConfirmationNumber = data['applicationConfirmationNumber'] || 'N/A';
+      this.about.publicationNumber = data['publicationNumber'] || 'N/A';
+      this.about.patentNumber = data['patentNumber'] || 'N/A';
+      this.about.attorneyDocketNumber = data['attorneyDocketNumber'] || 'N/A';
+      this.about.foreignPriorityClaimed = data['foreignPriorityClaimed'] || 'N/A';
+      this.about.firstInventorToFileIndicator = data['firstInventorToFileIndicator'] || 'N/A';
+      this.about.businessEntityStatusCategory = data['businessEntityStatusCategory'] || 'N/A';
+      // this.about.docketCategory = data['docketCategory'] || 'N/A';
+      // this.about.docketCategory = data['docketCategory'] || 'N/A';
+      // this.about.docketCategory = data['docketCategory'] || 'N/A';
+    });
   }
 
 }
---
+++ src/app/services/about.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import aboutData from '../../assets/data/link1.json';
+import { Observable } from 'rxjs';
+import { of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AboutService {
+  constructor() {
+  }
+
+  getAboutData() {
+    return of(aboutData);
+  }
+}
